88a38058c9add4181e08d17e9bed14e4f98c9b2d
Antonio Pons <toninopons@gmail.com>
2022-12-21 09:58:04 +0100

    Done.

---
+++ src/components/UncontrolledLogin.js
@@ -0,0 +1,56 @@
+import React, { createRef } from "react";
+
+export class UncontrolledLogin extends React.Component {
+    _submitBtnRef = createRef();
+    _userRef = createRef();
+    _passRef = createRef();
+
+    handleFormSubmit = (event) => {
+        event.preventDefault();
+        const username = event.target.elements.username.value;
+        const password = event.target.elements.password.value;
+        const rememeber = event.target.elements.remember.checked;
+        
+        if (username !== '' && password !== '') {
+            console.log({
+                username,
+                password,
+                rememeber
+            });
+        }
+    }
+
+    handleInputChange = (event) => {
+        event.preventDefault();
+
+        if (this._userRef.current.value !== '' && this._passRef.current.value)
+            this._submitBtnRef.current.disabled = false;
+        else 
+            this._submitBtnRef.current.disabled = true;
+    }
+    
+    disableSubmitBtn = () => {
+        if (this._submitBtnRef.current.disabled === false)
+            this._submitBtnRef.current.disabled = true;
+    }
+    
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleFormSubmit} >
+                    <label htmlFor="username">Username:</label>
+                    <input ref={this._userRef} onChange={this.handleInputChange} name='username' type='text' id='username' autoComplete="off"/>
+                    <br />
+                    <label htmlFor="password">Password:</label>
+                    <input ref={this._passRef} onChange={this.handleInputChange} name='password' type='password' id='password'/>
+                    <br />
+                    <label htmlFor="remember">Remember me:</label>
+                    <input name='remember' type='checkbox' id="remember"/>
+                    <br />
+                    <button disabled={true} ref={this._submitBtnRef} type='submit'>login</button>
+                    <button disabled={false}  onClick={this.disableSubmitBtn} type='reset'>reset</button>
+                </form>
+            </div>
+        );
+    }
+}
