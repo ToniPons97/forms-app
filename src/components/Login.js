53a655f886609f839ad507f74a2c08ca6cb7a7b1
Antonio Pons <toninopons@gmail.com>
2023-01-03 05:36:49 +0100

    Done.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,40 +1,65 @@
 import React, { useState } from "react";
 
 export const Login = () => {
+    /*
     const [username, setUserName] = useState('');
     const [password, setPassword] = useState('');
     const [remember, setRemember] = useState(false);
+    */
+    const [form, setForm] = useState({username: '', password: '', remember: false});
 
     const handleInputChange = (event) => {
-        const value = event.target.value;
-        const name = event.target.name;
+        const { name, value } = event.target;
 
-        if (name === 'username')
-            setUserName(value);
-        if (name === 'password')
-            setPassword(value);
-        if (name === 'remember')
-            setRemember(!remember);
+        setForm(prevForm => ({
+            ...prevForm,
+            [name]: name === 'remember' ? !prevForm.remember : value
+        }));
+
+
+        
+        
+        console.log(form);
     }
 
-    const onLogin = () => console.log({username, password, remember});
+    const onLogin = (event) => {
+        event.preventDefault();
+        console.log(form);
+    }
 
     return (
-        <div>
+        <form>
             <label htmlFor='username'>Username: </label>
-            <input type='text' name='username' id='username' onChange={handleInputChange} autoComplete='off'/>
+            <input 
+                type='text' 
+                name='username' 
+                id='username' 
+                value={form.username} 
+                onChange={handleInputChange} 
+                autoComplete='off'
+            />
             <br />
             <label htmlFor='password'>Password: </label>
-            <input type='password' name='password' id='password' onChange={handleInputChange} />
+            <input 
+                type='password' 
+                name='password' 
+                id='password' 
+                value={form.password}
+                onChange={handleInputChange} />
             <br />
             <label htmlFor='remember'>remember me</label>
-            <input type='checkbox' name='remember' id='remember' onChange={handleInputChange}/>
+            <input 
+                type='checkbox' 
+                name='remember' 
+                id='remember' 
+                value={form.remember}
+                onChange={handleInputChange}/>
             <br />
             <button id='login-btn' 
-                disabled={username === '' && password === ''}
+                disabled={form.username === '' && form.password === ''}
                 onClick={onLogin}    
             >Login</button>
-        </div>
+        </form>
     );
 }
 
